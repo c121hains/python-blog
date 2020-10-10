7892eaa30bc3af4be16de28f07a32d47804e3d27
Rob Wilkinson <c121hains@gmail.com>
2020-10-10 14:55:53 -0700

    Add flask origin

---
+++ microblog.py
@@ -0,0 +1,8 @@
+
+#'Flask shell' calls this make_shell_context()
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
